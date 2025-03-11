057561d4f9bf9e689ada7e6deda838b6cb684049
Ahmed Shawki <ahmedshawki2026@gmail.com>
2025-03-11 23:02:10 +0200

    data base

---
+++ DataAccessLayer/Entities/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Payment
+{
+    [Key]
+    public int Payment_ID { get; set; }
+
+    [Required]
+    public int Enrollment_ID { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string Payment_method { get; set; }
+
+    public DateTime Transaction_date { get; set; }
+
+    [ForeignKey("Enrollment_ID")]
+    public Enrollment Enrollment { get; set; }
+}
---
+++ DataAccessLayer/Entities/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+public class Role
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string RoleName { get; set; }
+
+
+    public ICollection<User> Users { get; set; }
+}
